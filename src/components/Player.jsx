cef0dc0e2a5966ddb14578a60c5bb4796d2b806e
Irmina Kalinowska <irminakalinowska0@gmail.com>
2024-01-22 20:49:14 +0100

    gameboard

---
+++ src/components/Player.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useState } from 'react';
+
+const Player = ({ initialName, symbol }) => {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleClick = () => {
+        setIsEditing((editing) => !editing);
+    };
+    const handleChange = ({ target }) => {
+        setPlayerName(target.value);
+    };
+
+    let editablePlayerName = <span className='player-name'>{playerName}</span>;
+
+    if (isEditing) {
+        editablePlayerName = (
+            <input
+                type='text'
+                value={playerName}
+                onChange={handleChange}
+                required
+            />
+        );
+    }
+
+    return (
+        <li>
+            <span className='player'>{editablePlayerName}</span>
+            <span className='player-symbol'>{symbol}</span>
+            <button onClick={handleClick}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
+    );
+};
+
+export default Player;
